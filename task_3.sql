3da5863e5e2fbcafd1787cafd367a122f1abd4d8
oumar <omaroutil@gmail.com>
2025-10-16 20:03:53 +0100

    Explicitly select alx_book_store database
    Added a USE statement to select the alx_book_store database explicitly.

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,5 @@
+-- Explicitly select the alx_book_store database
+USE alx_book_store;
+
 -- List all tables in the current database context.
--- Assumes the 'alx_book_store' database is selected or specified 
--- as an argument to the mysql command.
 SHOW TABLES;
